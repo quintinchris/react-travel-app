7488ae56f8700592754c73b9ed61007f4a175a23
QuintinChris <cwq23drexel@gmail.com>
2020-06-19 08:58:53 -0400

    added get route for all entries

--- server/src/routes/catalog.js
+++ server/src/routes/catalog.js
@@ -3,10 +3,13 @@ const LogEntry = require('../models/LogEntry');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.json({
-    message: 'Hey its /',
-  });
+router.get('/', async (req, res, next) => {
+  try {
+    const entries = await LogEntry.find();
+    res.json(entries);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/', async (req, res, next) => {
@@ -15,6 +18,9 @@ router.post('/', async (req, res, next) => {
     const createdEntry = await logEntry.save();
     res.json(createdEntry);
   } catch (error) {
+    if (error.name === 'ValidationError') {
+      res.status(422);
+    }
     next(error);
   }
 });
